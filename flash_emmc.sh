f7c20d1ab73d69488c16787a27736c5d1e959943
avafinger <alex.mobigo@gmail.com>
2017-05-05 16:15:52 -0300

    ensure sudo and make sure the special partition is available

--- flash_emmc.sh
+++ flash_emmc.sh
@@ -32,23 +32,33 @@ umount /dev/mmcblk0* >/dev/null 2>&1
 sleep 1
 sync
 
+sudo partprobe
+sleep 2
+sync
+sudo partprobe ${SDCARD}
+sleep 2
+
+
 set -e
 pt_info "Writing to eMMC, please wait..."
 dd if=./boot0.bin conv=notrunc bs=1k seek=8 of=/dev/mmcblk0
 dd if=./ub-m64-emmc.bin conv=notrunc bs=1k seek=19096 of=/dev/mmcblk0
 
 mkdir -p erootfs
-mount /dev/mmcblk0p2 erootfs
+sudo partprobe ${SDCARD}
+sleep 4
+sudo mount /dev/mmcblk0p2 erootfs
 tar -xvpzf rootfs_m64_rc3.tar.gz -C ./erootfs --numeric-ow
 sync
-umount erootfs
+sudo umount erootfs
 rm -fR erootfs
 
 mkdir eboot
-mount /dev/mmcblk0p1 eboot
-tar -xvzf boot_m64_rc3.tar.gz -C ./eboot
+sudo mount /dev/mmcblk0p1 eboot
+tar -xvzf boot_m64_rc3.tar.gz -C ./eboot --no-same-owner
 sync
-umount eboot
+sudo umount eboot
 rm -fR eboot
 sync
 pt_info "Finished eMMC, reboot without the SD CARD!"
+pt_ok "run: sudo shutdown -h now , remove the SD card and power the board again. Enjoy!"
--- flash_sd.sh
+++ flash_sd.sh
@@ -41,25 +41,37 @@ umount ${SDCARD}* >/dev/null 2>&1
 sleep 1
 sync
 
+sudo partprobe
+sleep 2
+sync
+sudo partprobe ${SDCARD}
+sleep 2
+
+
 set -e
 
 pt_warn "Flashing $SDCARD...."
 dd if=./boot0.bin conv=notrunc bs=1k seek=8 of=${SDCARD}
 dd if=./ub-m64-sdcard.bin conv=notrunc bs=1k seek=19096 of=${SDCARD}
 
+pt_info "Decompressing rootfs to $SDCARD"2", please wait... (takes some time)"
 mkdir -p erootfs
-mount $SDCARD"2" erootfs
+sudo partprobe ${SDCARD}
+sleep 4
+sudo mount $SDCARD"2" erootfs
 tar -xvpzf rootfs_m64_rc3.tar.gz -C ./erootfs --numeric-ow
 sync
-umount erootfs
+sudo umount erootfs
 rm -fR erootfs
 
 set +e
 mkdir eboot
-mount $SDCARD"1" eboot
-tar -xvpzf boot_m64_rc3.tar.gz -C ./eboot
+sudo mount $SDCARD"1" eboot
+tar -xvpzf boot_m64_rc3.tar.gz -C ./eboot  --no-same-owner
 sync
-umount eboot
+sudo umount eboot
 rm -fR eboot
 
 pt_ok "Finished flashing $SDCARD!"
+pt_ok "You can remove the SD card and boot up on your board. Enjoy!"
+
